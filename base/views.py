8b669ba8258bc4c5b6db2a870a9c8ebb24b7dcaf
collins kiprutoh <kiprutohcollo@gmail.com>
2022-06-09 00:32:42 +0300

    created url files and migrations

--- base/views.py
+++ base/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def tasklist(request):
+    return HttpResponse('To Do list')
